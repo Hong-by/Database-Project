646790c72cce7b0e5132c76f8c7c8cac29f287b7
HongBY <hby033@naver.com>
2021-07-15 18:36:42 +0900

    07/15 상품 등록 페이지 작업 중

--- js/jq.main.js
+++ js/jq.main.js
@@ -18,13 +18,40 @@ $(function(){
 
   headerStick();
 
-  $(".mobile_menu").click(function(){
-    $(this).toggleClass("on");
-    if($(this).hasClass("on")){
-      $(".menu_items").slideDown(250);
-    } else {
-      $(".menu_items").slideUp(250);
+  // navigation slide down and up when mobile menu click
+  const barsClick = function(){
+    $(".mobile_menu").click(function(){
+      $(this).toggleClass("on");
+      if($(this).hasClass("on")){
+        $(".menu_items").slideDown(250);
+      } else {
+        $(".menu_items").slideUp(250);
+      }
+    });
+  }
+  barsClick();
+
+  // index page description text cut
+  const cuttingText = function(){
+    // console.log($(".featured_item").lenght);
+    for(let i = 0; i < $(".featured_item").length; i++){
+      const textLength = $(".featured_item").eq(i).find("p.desc").text();
+      // console.log(textLength);
+
+      $(".featured_item").eq(i).find("p.desc").text(textLength.substr(0, 60) + "...");
     }
-  });
+  }
+  cuttingText();
+
+  // index page items load more
+  const loadMore = function(){
+    $(".featured_item").hide();
+    $(".featured_item").slice(0, 3).show();
+
+    $(".load_more button").click(function(){
+      $(".featured_item:hidden").slice(0, 3).show();
+    });
+  }
+  loadMore();
 
 });
