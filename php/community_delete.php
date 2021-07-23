175656fe50d97d2aa05a6ff85e0f0f3e3bc44cda
HongBY <hby033@naver.com>
2021-07-23 14:51:32 +0900

    07/23 게시판 기능 구현 완료 / 게시판 검색기능 구현 예정

---
+++ php/community_delete.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+if(isset($_SESSION['userid'])){
+  $userid = $_SESSION['userid'];
+} else {
+  $userid = "";
+}
+
+$deail_writer = $_GET['deail_writer'];
+$detail_idx = $_GET['detail_idx'];
+
+
+if(!$userid || $userid != $deail_writer){
+  echo "
+  <script>
+    alert('잘못된 접근입니다.');
+    location.href='/zay/index.php';
+  </script>
+  ";
+} else {
+  include $_SERVER["DOCUMENT_ROOT"]."/connect/db_conn.php";
+  $sql = "DELETE FROM zay_comm WHERE ZAY_comm_idx=$detail_idx";
+
+  mysqli_query($dbConn, $sql);
+
+  echo "
+    <script>
+      alert('삭제가 완료되었습니다.');
+      location.href='/zay/pages/menu_page/community_form.php';
+    </script>
+  ";
+
+};
+
+?>
